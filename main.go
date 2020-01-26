f3a7fa808c8af091036e8dde2e9e54a7a36aa267
sitthakarn <sitthakarn.p@gmail.com>
2020-01-27 00:20:25 +0700

    init commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// main function of go
+func main() {
+	http.Handle("/", loggingMiddleware(http.HandlerFunc(handler)))
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "package main #14")
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Infof("url: %s", r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
